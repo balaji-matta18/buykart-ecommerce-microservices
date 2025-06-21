1c4f900e7e401030252dc7416f5a3105a0e4b542
balaji <balajim18621@gmail.com>
2025-06-21 12:57:15 +0530

    Initial commit for BuyKart E-commerce Microservices project

---
+++ order-service/src/main/java/com/balaji/orderservice/dto/OrderRequest.java
@@ -0,0 +1,15 @@
+package com.balaji.orderservice.dto;
+
+import com.balaji.orderservice.model.OrderLineItems;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    private List<OrderLineItemsDto> orderLineItemsDtoList;
+}
